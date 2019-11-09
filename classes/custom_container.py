9efd0e5e09c0cf9862156576a87449d94f2a8349
Nicholas Black <nicholas.c.black@leidos.com>
2019-11-09 14:04:15 -0500

    some updates related to private members

--- classes/custom_container.py
+++ classes/custom_container.py
@@ -1,33 +1,35 @@
 class TagCloud:
     # define a constructor
     def __init__(self):
-        self.tags = {}
+        self.__tags = {}
 
     def add(self, tag):
-        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+        self.tags[tag.lower()] = self.__tags.get(tag.lower(), 0) + 1
 
     def __getitem__(self, tag):
-        return self.tags.get(tag.lower(), 0)
+        return self.__tags.get(tag.lower(), 0)
 
     def __setitem__(self, tag, count):
         self.tags[tag.lower()] = count
 
     def __len__(self):
-        return len(self.tags)
+        return len(self.__tags)
 
     def __iter__(self):
         # returns an iterator object which gives us one item at a time in a for loop
-        return iter(self.tags)
+        return iter(self.__tags)
 
 
 cloud = TagCloud()
 
 cloud["python"] = 10
 len(cloud)
+# we made it private by prefixing with '__'
+# print(cloud.__tags)
 cloud.add("Python")
 cloud.add("python")
 cloud.add("python")
-print(cloud.tags)
+print(cloud.__tags)
 
 
 # operations supported by this custom container type
@@ -36,3 +38,7 @@ print(cloud.tags)
 # cloud["python"] = 10 # get an item by its key
 # for tag in cloud:
 #     print(tag)
+
+"""
+    In Python unlike C# or Java we don't really have the concept of private members.  These private members are still accessible from the outside.  Using double underscores is more convention to prevent accidental access of these private members.
+"""
